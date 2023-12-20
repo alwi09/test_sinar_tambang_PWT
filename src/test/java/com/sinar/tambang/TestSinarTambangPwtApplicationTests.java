4395527cd706a6cec04b21b7443be4576eed707f
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-12-20 21:14:14 +0700

    [FEATURE] initial commit

---
+++ src/test/java/com/sinar/tambang/TestSinarTambangPwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinar.tambang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSinarTambangPwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
